fe5fb3d39f38ea31cf29c616cdfcd0d2be26bfe2
MadTown86 <101869180+MadTown86@users.noreply.github.com>
2022-08-22 05:53:55 -0500

    Continuing to flesh out new grading mechanism and print to excel process.

--- sscpackage/grade_gtltyoyssc.py
+++ sscpackage/grade_gtltyoyssc.py
@@ -2,17 +2,33 @@
 Grade the Income Statements
 """
 
-import gradeparsecombinessc
+import gradesheetprintssc
 
-class GTLTYoYSSC():
+class GTLTYoYSSC(gradesheetprintssc.GradeSheetPrintSSC):
+    def __init__(self):
+        super().__init__()
+        self.sectionname = 'GTLT'
+        self.increasingsections = ["Total Revenue", "Net Income", "Gross Margin", "Operatng Margin", "Net Margin",
+                                   "Gross Profit", "EBIT", "Operating Income", "Net Income From Continuing Ops",
+                                   "Income Before Tax", "Research & Development", "Total Assets", "Retained Earnings"]
+        self.decreasingsections = ["Selling, General & Administrative", "Other Operating Expenses", "Interest Expense",
+                                   "Total Operating Expenses", "Cost Of Revenue", "Total Other Income Expense Net"]
 
-    """
-    Task List:
-    1.
-    """
     def gtltmetricsgradessc(self, ticker, parsecombossc, uniqueidssc, awardsystemssc):
-        localawardsection = awardsystemssc['GTLT']
+        runningtotalgtlt = {}
+        localawardsectiongtlt = awardsystemssc['GTLT']
+        localincdatssc = parsecombossc['incdat']
+        localbaldatssc = parsecombossc['baldat']
+        localavoidmetricgtltinc = parsecombossc["incdatqual"]
+        localavoidmetricgtltbal = parsecombossc["baldatqual"]
+        runsectorssc = parsecombossc["Sector"]
+        runindustryssc = parsecombossc["Industry"]
+        self.printprimer(self.sectionname, ticker, uniqueidssc, runsectorssc, runindustryssc)
 
-
-
-    pass
+        for nameval in self.increasingsections:
+            if nameval in localavoidmetricgtltinc:
+                """Write the method of calculating with only available data/years combo"""
+                continue
+            else:
+                for yearnumberchrono in range(len(localincdatssc)):
+                    pass
--- sscpackage/gradesheetprintssc.py
+++ sscpackage/gradesheetprintssc.py
@@ -1,8 +1,27 @@
 """
 Superclass - adding data to Grade Sheet method
 """
+import pandas
+import datetime
 
 class GradeSheetPrintSSC():
-    def gdpssc(self):
-        #  Placeholder while I work on grading portion first
-        pass
+    def __init__(self):
+        self.gradesheetprinterprimer = {}
+        self.gradesheetprinter = {}
+        self.permpathtoexcelssc = r'C:\SSC\SimpleStockChecker_REV1\sscpackage\storage\excelstorage\\'
+
+    def printprimer(self, sectionname, ticker, uniquerunid, sectorssc, industryssc):
+        curdatessc = datetime.datetime.today().strftime("%d_%m_%y")
+        self.gradesheetprinterprimer.update({"Ticker": str(ticker), "Unique Run ID": uniquerunid,
+                                             "Sector": sectorssc, "Industry": industryssc,
+                                             "Date / Time": datetime.datetime.today().strftime("%d-%m%y  %H:%M:%S"),
+                                             "Section": str(sectionname)})
+        localdfssc = pandas.DataFrame(self.gradesheetprinterprimer)
+        gradeprimepathssc = self.permpathtoexcelssc + '__' + str(ticker) + '__' + str(uniquerunid)
+        with pandas.ExcelWriter(path=gradeprimepathssc, mode='w') as exwssc:
+            localdfssc.to_excel(exwssc, sheet_name=str(ticker) + '__' + str(curdatessc))
+
+
+    def gradeprinterssc(self, **kwargs):
+        self.gradesheetprinter.update(kwargs)
+
