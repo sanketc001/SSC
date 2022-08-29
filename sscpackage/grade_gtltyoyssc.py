8268d51c82064e52e64e09428f644b419dd1eefd
MadTown86 <101869180+MadTown86@users.noreply.github.com>
2022-08-29 05:47:55 -0500

    GTLT for increasing portions works, need to flesh out other grading factors and mechanisms.

--- sscpackage/grade_gtltyoyssc.py
+++ sscpackage/grade_gtltyoyssc.py
@@ -1,18 +1,20 @@
 """
 Grade the Income Statements
 """
-
+import grade_gtltyoyssc
 import gradesheetprintssc
 
 class GTLTYoYSSC(gradesheetprintssc.GradeSheetPrintSSC):
     def __init__(self):
         super().__init__()
         self.sectionname = 'GTLT'
-        self.increasingsections = ["Total Revenue", "Net Income", "Gross Margin", "Operatng Margin", "Net Margin",
+        self.increasingsections = ["Total Revenue", "Net Income", "Gross Margin", "Operating Margin", "Net Margin",
                                    "Gross Profit", "EBIT", "Operating Income", "Net Income From Continuing Ops",
                                    "Income Before Tax", "Research & Development", "Total Assets", "Retained Earnings"]
         self.decreasingsections = ["Selling, General & Administrative", "Other Operating Expenses", "Interest Expense",
                                    "Total Operating Expenses", "Cost Of Revenue", "Total Other Income Expense Net"]
+        self.printerdictssc = {}
+        self.gradestore = {}
 
     def gtltmetricsgradessc(self, ticker, parsecombossc, uniqueidssc, awardsystemssc):
         runningtotalgtlt = {}
@@ -54,96 +56,146 @@ class GTLTYoYSSC(gradesheetprintssc.GradeSheetPrintSSC):
             print(weightind)
             respointrunner = 0
             if statement == "incdat":
-                stringerbegin = """
-                            {nameval} : Available Years:: {count} ::
-                            Category:: {statement}
-
-                            Grade Statements Follow:
-                            """.format(nameval=nameval, count=count, statement=statement)
-                stringermid = ""
                 sourcevalsinc = localincdatssc[nameval]
                 for indexno in range(count-1, 0, -1):
+                    keyforprint = "{nameval} - Year {yearnew} | Year {yearold}".format(nameval=nameval,
+                                                                                       yearnew=indexno - 1,
+                                                                                       yearold=indexno)
                     if sourcevalsinc[indexno-1] > sourcevalsinc[indexno]:
-                        stringermid += "\nYear {indexnoplus}: {indexnoplusvalue} is '>' Year {indexnoval}" \
-                                             " :: Year {indexno} ---* + {pointsper} Point".format(indexnoplus= indexno-1,
-                                                                         indexnoplusvalue=sourcevalsinc[indexno-1],
-                                                                         indexnoval=sourcevalsinc[indexno],
-                                                                         indexno=indexno,
-                                                                         pointsper=pointsper)
                         respointrunner += pointsper
+                        inlinesymbol = ">"
                     else:
-                        stringermid += "\nYear {indexnoplus}: {indexnoplusvalue} is '<' Year {indexnoval}" \
-                                             " :: Year {indexno} ---* + {pointsper} Point".format(indexnoplus= indexno-1,
-                                                                                                  indexnoplusvalue=sourcevalsinc[indexno-1],
-                                                                                                  indexnoval=sourcevalsinc[indexno],
-                                                                                                  indexno=indexno,
-                                                                                                  pointsper=pointsper)
-                        continue
+                        inlinesymbol = "<"
+
+                    valueforprint = "{yearnew} {inlinesymbol} {yearold} POINTS " \
+                                    "{respointrunner} POINTVAL {pointsper}".format(yearnew=sourcevalsinc[indexno-1],
+                                                                                   inlinesymbol=inlinesymbol,
+                                                                                   yearold=sourcevalsinc[indexno],
+                                                                                   respointrunner=respointrunner,
+                                                                                   pointsper=pointsper).split(" ")
+                    self.printerdictssc[str(keyforprint)] = valueforprint
 
                 respointrunner *= weightind
                 restotpoints = 1 * (count-1)
                 restotpoints *= weightind
-                stringerend = "End Of Grading Element: {nameval}\nPoints Per Grade Element = {pointsper}\n" \
-                              "Total Current Points = {respointrunner} ::: " \
-                              "Weight = {weightind}".format(nameval=nameval, pointsper=pointsper, respointrunner=respointrunner, weightind=weightind)
-                stringertotal = stringerbegin + stringermid + stringerend
                 runningtotalgtlt[nameval] = {"Base Points": restotpoints, "Current Points": respointrunner}
-                return stringertotal
 
             elif statement == "baldat":
-                stringerbegin = """
-                                            {nameval} : Available Years:: {count} ::
-                                            Category:: {statement}
-
-                                            Grade Statements Follow:
-                                            """.format(nameval=nameval, count=count, statement=statement)
-                stringermid = ""
                 sourcevalsbal = localbaldatssc[nameval]
                 for indexno in range(count-1, 0, -1):
+                    keyforprint = "{nameval} - Year {yearnew} | Year {yearold}".format(nameval=nameval,
+                                                                                       yearnew=indexno - 1,
+                                                                                       yearold=indexno)
                     if sourcevalsbal[indexno-1] > sourcevalsbal[indexno]:
-                        stringermid += "\nYear {indexnoplus}: {indexnoplusvalue} is '>' Year {indexnoval}" \
-                                       " :: Year {indexno} ---* + {pointsper} Point".format(indexnoplus= indexno-1,
-                                                                                            indexnoplusvalue=sourcevalsbal[indexno-1],
-                                                                                            indexnoval=sourcevalsbal[indexno],
-                                                                                            indexno=indexno,
-                                                                                            pointsper=pointsper)
                         respointrunner += pointsper
+                        inlinesymbol = ">"
                     else:
-                        stringermid += "\nYear {indexnoplus}: {indexnoplusvalue} is '<' Year {indexnoval}" \
-                                       " :: Year {indexno} ---* + {pointsper} Point".format(indexnoplus=indexno - 1,
-                                                                                            indexnoplusvalue=
-                                                                                            sourcevalsbal[indexno - 1],
-                                                                                            indexnoval=sourcevalsbal[
-                                                                                                indexno],
-                                                                                            indexno=indexno,
-                                                                                            pointsper=pointsper)
-                        continue
+                        inlinesymbol = "<"
+
+                    valueforprint = "{yearnew} {inlinesymbol} {yearold} POINTS " \
+                                    "{respointrunner} POINTVAL {pointsper}".format(yearnew=sourcevalsbal[indexno-1],
+                                                                                   inlinesymbol=inlinesymbol,
+                                                                                   yearold=sourcevalsbal[indexno],
+                                                                                   respointrunner=respointrunner,
+                                                                                   pointsper=pointsper).split(" ")
+                    self.printerdictssc[keyforprint] = valueforprint
 
                 respointrunner *= weightind
                 restotpoints = 1 * (count-1)
                 restotpoints *= weightind
-                stringerend = "End Of Grading Element: {nameval}\nPoints Per Grade Element = {pointsper}\n" \
-                              "Total Current Points = {respointrunner} ::: " \
-                              "Weight = {weightind}".format(nameval=nameval, pointsper=pointsper, respointrunner=respointrunner, weightind=weightind)
-                stringertotal = stringerbegin + stringermid + stringerend
                 runningtotalgtlt[nameval] = {"Base Points": restotpoints, "Current Points": respointrunner}
-                return stringertotal, runningtotalgtlt
+
+            return runningtotalgtlt
+
+        def decreasinggtltssc(nameval, count, statement):
+            pointsper = int(localawardsectiongtlt[nameval]["points"])
+            weightind = int(localawardsectiongtlt[nameval]["weight"])
+            print(pointsper)
+            print(weightind)
+            respointrunner = 0
+            if statement == "incdat":
+                sourcevalsinc = localincdatssc[nameval]
+                for indexno in range(count-1, 0, -1):
+                    keyforprint = "{nameval} - Year {yearnew} | Year {yearold}".format(nameval=nameval,
+                                                                                       yearnew=indexno - 1,
+                                                                                       yearold=indexno)
+                    if sourcevalsinc[indexno-1] < sourcevalsinc[indexno]:
+                        respointrunner += pointsper
+                        inlinesymbol = ">"
+                    else:
+                        inlinesymbol = "<"
+
+                    valueforprint = "{yearnew} {inlinesymbol} {yearold} POINTS " \
+                                    "{respointrunner} POINTVAL {pointsper}".format(yearnew=sourcevalsinc[indexno-1],
+                                                                                   inlinesymbol=inlinesymbol,
+                                                                                   yearold=sourcevalsinc[indexno],
+                                                                                   respointrunner=respointrunner,
+                                                                                   pointsper=pointsper).split(" ")
+                    self.printerdictssc[str(keyforprint)] = valueforprint
+
+                respointrunner *= weightind
+                restotpoints = 1 * (count-1)
+                restotpoints *= weightind
+                runningtotalgtlt[nameval] = {"Base Points": restotpoints, "Current Points": respointrunner}
+
+            elif statement == "baldat":
+                sourcevalsbal = localbaldatssc[nameval]
+                for indexno in range(count-1, 0, -1):
+                    keyforprint = "{nameval} - Year {yearnew} | Year {yearold}".format(nameval=nameval,
+                                                                                       yearnew=indexno - 1,
+                                                                                       yearold=indexno)
+                    if sourcevalsbal[indexno-1] < sourcevalsbal[indexno]:
+                        respointrunner += pointsper
+                        inlinesymbol = ">"
+                    else:
+                        inlinesymbol = "<"
+
+                    valueforprint = "{yearnew} {inlinesymbol} {yearold} POINTS " \
+                                    "{respointrunner} POINTVAL {pointsper}".format(yearnew=sourcevalsbal[indexno-1],
+                                                                                   inlinesymbol=inlinesymbol,
+                                                                                   yearold=sourcevalsbal[indexno],
+                                                                                   respointrunner=respointrunner,
+                                                                                   pointsper=pointsper).split(" ")
+                    self.printerdictssc[keyforprint] = valueforprint
+
+                respointrunner *= weightind
+                restotpoints = 1 * (count-1)
+                restotpoints *= weightind
+                runningtotalgtlt[nameval] = {"Base Points": restotpoints, "Current Points": respointrunner}
+
+            return runningtotalgtlt
+
 
         outputstoressc = {}
         for nameval in self.increasingsections:
-            print(nameval)
-            print(isenoughdatassc(nameval))
             if isenoughdatassc(nameval):
                 valueholderlist = [*isenoughdatassc(nameval)]
-                print(valueholderlist)
                 if len(valueholderlist) == 3:
-                    outputstoressc[nameval] = [increasinggtltssc(*valueholderlist)]
+                    self.gradestore.update(increasinggtltssc(*valueholderlist))
             else:
                 continue
 
+        self.gradeprinterssc(**self.printerdictssc)
+        self.sectionprinttoexcel()
+        self.sectionendprinttoexcel(**self.gradestore)
+
         return outputstoressc
 
+
+
 if __name__ == "__main__":
-    pass
+    import gradeparsecombinessc
+    import awardsystemssc
+    testlogvaridssc = 'NVDA__Y8bdxbfeWiliz3B'
+    ticker, uniqueid = testlogvaridssc.split("__")
+    AWS = awardsystemssc.AwardSystemSSC()
+    awardsystempassin = AWS.fetchawardsystem("Industry", "Sector")
+
+    GPSSC = gradeparsecombinessc.GradeParseCombineSSC()
+    gradeparsecombo = GPSSC.gradeparsecombinessc(ticker, uniqueid)['NVDA__Y8bdxbfeWiliz3B']
+
+    GTLT = grade_gtltyoyssc.GTLTYoYSSC()
+    GTLT.gtltmetricsgradessc(ticker, gradeparsecombo, uniqueid, awardsystempassin)
+
 
     # TODO: add testing
--- sscpackage/gradesheetprintssc.py
+++ sscpackage/gradesheetprintssc.py
@@ -2,6 +2,8 @@
 Superclass - adding data to Grade Sheet method
 """
 import pandas
+import openpyxl
+import openpyxl.utils.dataframe
 import datetime
 
 class GradeSheetPrintSSC():
@@ -9,6 +11,8 @@ class GradeSheetPrintSSC():
         self.gradesheetprinterprimer = {}
         self.gradesheetprinter = {}
         self.permpathtoexcelssc = r'C:\SSC\SimpleStockChecker_REV1\sscpackage\storage\excelstorage\\'
+        self.instancepath = ""
+        self.logfilename = ""
 
     def printprimer(self, sectionname, ticker, uniquerunid, sectorssc, industryssc):
         curdatessc = datetime.datetime.today().strftime("%d_%m_%y")
@@ -16,15 +20,42 @@ class GradeSheetPrintSSC():
                                              "Sector": str(sectorssc), "Industry": str(industryssc),
                                              "Date / Time": datetime.datetime.today().strftime("%d-%m-%y  %H:%M:%S"),
                                              "Section": str(sectionname)})
-        localdfssc = pandas.DataFrame(self.gradesheetprinterprimer, index=["DATA"])
-        gradeprimepathssc = str(self.permpathtoexcelssc) + str(ticker) + '__' + str(uniquerunid) + ".xlsx"
-        with pandas.ExcelWriter(path=gradeprimepathssc, mode='w') as exwssc:
+        localdfssc = pandas.DataFrame.from_dict(self.gradesheetprinterprimer, orient='index')
+        self.logfilename = str(ticker) + '__' + str(uniquerunid) + ".xlsx"
+        self.instancepath = str(self.permpathtoexcelssc) + str(self.logfilename)
+        self.sheetnamesscgr = str(ticker) + '__' + str(curdatessc)
+        with pandas.ExcelWriter(path=self.instancepath, engine='xlsxwriter', mode='w') as exwssc:
             localdfssc.to_excel(exwssc, sheet_name=(str(ticker) + '__' + str(curdatessc)))
 
-            exwssc.close()
-
-
-    def gradeprinterssc(self, **kwargs):
-        self.gradesheetprinter.update(kwargs)
+    def gradeprinterssc(self, **data):
+        self.gradesheetprinter.update(**data)
         # TODO: flesh out printing data to excel and learn more about numpy/pandas its NUMPIE not frunumpy
+
+    def sectionprinttoexcel(self):
+        sectiondf = pandas.DataFrame.from_dict(self.gradesheetprinter, orient="index")
+        wbsscgr = openpyxl.load_workbook(self.instancepath)
+        wssscgr = wbsscgr[self.sheetnamesscgr]
+        for rowitem in openpyxl.utils.dataframe.dataframe_to_rows(sectiondf, index=True, header=False):
+            wssscgr.append(rowitem)
+
+        wbsscgr.save(self.instancepath)
+
+    def sectionendprinttoexcel(self, **kwargs):
+        localpoint = 0
+        localtotal = 0
+        localdict = kwargs
+        for key in localdict.keys():
+            localpoint += localdict[key]["Current Points"]
+            localtotal += localdict[key]["Base Points"]
+
+        statementstring = {"TOTAL CURRENT POINTS": localpoint, "TOTAL POSSIBLE POINTS": localtotal}
+
+        sectionend = pandas.DataFrame.from_dict(statementstring, orient="index")
+        wb = openpyxl.load_workbook(self.instancepath)
+        wssscgr = wb[self.sheetnamesscgr]
+        for rowitem in openpyxl.utils.dataframe.dataframe_to_rows(sectionend, index=True, header=False):
+            wssscgr.append(rowitem)
+
+        wb.save(self.instancepath)
+
 
